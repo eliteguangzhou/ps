8a0620550bb657205df12144e13f6c85bd093948
tidjean <tidjean@gmail.com>
2012-09-06 17:15:54 +0800

    site en maintenance

--- includes/application_top.php
+++ includes/application_top.php
@@ -1,4 +1,5 @@
 <?php
+die('Site en maintenance');
 /*
  $Id: $
 
@@ -12,11 +13,12 @@
 
 // start the timer for the page parse time log
 define('PAGE_PARSE_START_TIME', microtime());
-error_reporting(E_ALL);
+/*error_reporting(E_ALL);
 // set the level of error reporting
 ini_set('display_errors', true);
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 error_reporting(1);
+*/
 // check support for register_globals
 if (function_exists('ini_get') && (ini_get('register_globals') == false) && (PHP_VERSION < 4.3) ) {
 	exit('Server Requirement Error: register_globals is disabled in your PHP configuration. This can be enabled in your php.ini configuration file or in the .htaccess file in your catalog directory. Please use PHP 4.3+ if register_globals cannot be enabled on the server.');
@@ -756,5 +758,4 @@ if (MAINTENANCE_ENABLE) {
 	if (!isset($_SESSION['ghj']))
 	die('Le site est actuellement en maintenance et sera de nouveau disponible dans quelques minutes.<br />Veuillez nous excuser pour la g&ecircne occasionn&eacute;e.');
 }
-error_log(LANGUAGE_CURRENCY);
 ?>
